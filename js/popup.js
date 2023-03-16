b7127a5f269be5f00203bc9c523743901f5b9dc1
Diana8991 <123815223+Diana8991@users.noreply.github.com>
2023-03-17 00:18:26 +0400

    Почти готовый проект
    Вопросы:
    1. Стрелка со странным фоном после поворота svg.
    2. Отступы между звездами.

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,10 @@ const popup = document.querySelector('.popup');
 const btn = document.querySelector('.banner_button');
 const close_btn = document.querySelector('.close');
 
+console.log('popup')
+console.log('btn')
+console.log('close_btn')
+
 btn.addEventListener('click', () => {
     popup.classList.add('open');
 })
